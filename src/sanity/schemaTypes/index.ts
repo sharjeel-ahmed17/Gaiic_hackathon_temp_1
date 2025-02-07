cff27ad945f814fe31a9fd8323a4a8cfd3f3f85c
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-02-08 02:28:26 +0500

    intrigate sanity data

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import products from './products'
+
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [products],
 }
