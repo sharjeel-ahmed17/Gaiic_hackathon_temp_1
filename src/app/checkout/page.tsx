3f64c3753716505dedd95fed28f87befcef7e92a
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-02-08 13:55:15 +0500

    checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -7,6 +7,19 @@ import { Products } from "../../../types/products"
 import {ChevronRight} from "lucide-react"
 
 const Checkout = () => {
+    const [cartItems , setCartItems] = useState<Products[]>([])
+
+    const [discount , setDiscount] = useState<Number>(0)
+    const [formValues , setFormValues] = useState({
+        firstName : "",
+        lastName : "",
+        addressName : "",
+        city : "",
+        zipCode  : "",
+        phone : "",
+        email : "",
+        firstName : "",
+    })
   return (
     <div>Checkout</div>
   )
